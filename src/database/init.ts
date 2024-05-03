8cca4e304448273b509890ee2f02a7f1737a81c3
kvargas-dev <kvargas@vumigroup.com>
2024-05-03 15:42:40 -0500

    remove select statement

--- src/database/init.ts
+++ src/database/init.ts
@@ -6,8 +6,7 @@ const DbInitializer = async () => {
     try {
         await Db.authenticate();
         UserModel.sync({ alter: false})
-        const result = await Db.query("SELECT 'connected'");
-        console.log('Connection to database initialized', result);
+        console.log('Connection to database initialized');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
