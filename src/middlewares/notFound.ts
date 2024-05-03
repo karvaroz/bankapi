10c7c48b2ef586a707fb10bbaedce556ba8bf080
kvargas-dev <kvargas@vumigroup.com>
2024-05-03 16:17:55 -0500

    not found route middleware

---
+++ src/middlewares/notFound.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404).json({ error: error.message });
+};
