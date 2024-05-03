42760bff48d66fd290aaac5064d774901a7acded
kvargas-dev <kvargas@vumigroup.com>
2024-05-03 14:27:53 -0500

    use user model database

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,8 +1,11 @@
+import UserModel from "../models/UserModel";
+
 import Db from "./index"
 
 const DbInitializer = async () => {
     try {
         await Db.authenticate();
+        UserModel.sync({ alter: false})
         const result = await Db.query("SELECT 'connected'");
         console.log('Connection to database initialized', result);
     } catch (error) {
