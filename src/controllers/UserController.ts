1cddd20df53f3e22e3bab7d18c8cf94154eb15ef
kvargas-dev <kvargas@vumigroup.com>
2024-05-17 15:44:23 -0500

    user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 import UserService from '../services/UserService'
+import { AccountStatus, EmailStatus, UserRole } from '../interfaces/enum/userEnum'
+import { IUserCreationBody } from '../interfaces/IUser'
 import Utility from '../utils/index.utils'
 import { ResponseCode } from '../interfaces/enum/codeEnum'
 
@@ -12,9 +15,33 @@ class UserController {
 
   async register(req: Request, res: Response) {
     try {
-      res.send({ res, req })
+      const params = { ...req.body }
+
+      const newUser: IUserCreationBody = {
+        firstname: params.firstname,
+        lastname: params.lastname,
+        email: params.email,
+        username: params.email.split('@')[0],
+        password: params.password,
+        role: UserRole.CUSTOMER,
+        isEmailVerified: EmailStatus.NOT_VERIFIED,
+        accountStatus: AccountStatus.ACTIVE,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      newUser.password = bcrypt.hashSync(newUser.password, 10)
+      let userExists = await this.userService.getUserByField({ email: newUser.email })
+
+      if (userExists) {
+        return Utility.handleError(res, 'User already exists', ResponseCode.ALREADY_EXIST)
+      }
+
+      let user = await this.userService.createUser(newUser)
+      user.password = ''
+
+      return Utility.handleSuccess(res, 'User created', { user }, ResponseCode.SUCCESS)
     } catch (error) {
-      res.send(error)
+      res.status(500).json({ message: 'Internal server error', error })
     }
   }
 
