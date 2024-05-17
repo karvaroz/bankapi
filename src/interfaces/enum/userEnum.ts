ad3371b54049d41cfa8a780dbca6250451816a3a
kvargas-dev <kvargas@vumigroup.com>
2024-05-17 15:44:50 -0500

    user enums

---
+++ src/interfaces/enum/userEnum.ts
@@ -0,0 +1,16 @@
+export const EmailStatus = {
+  VERIFIED: 'VERIFIED',
+  NOT_VERIFIED: 'NOT_VERIFIED'
+}
+
+export const UserRole = {
+  ADMIN: 'ADMIN',
+  CUSTOMER: 'CUSTOMER',
+  EDITOR: 'EDITOR'
+}
+
+export const AccountStatus = {
+  ACTIVE: 'ACTIVE',
+  INACTIVE: 'INACTIVE',
+  SUSPENDED: 'SUSPENDED'
+}
