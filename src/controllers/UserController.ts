5e92dc59127bd53c4ea3545f7a99390aea48c776
kvargas-dev <kvargas@vumigroup.com>
2024-05-17 14:27:07 -0500

    user middlewares

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,45 +1,46 @@
-import { Request, Response } from "express";
-import UserService from "../services/UserService";
-
+import { Request, Response } from 'express'
+import UserService from '../services/UserService'
+import Utility from '../utils/index.utils'
+import { ResponseCode } from '../interfaces/enum/codeEnum'
 
 class UserController {
-    private userService: UserService;
+  private userService: UserService
 
-    constructor(__userService:UserService) {
-        this.userService = __userService;
-    }
+  constructor(__userService: UserService) {
+    this.userService = __userService
+  }
 
-    async register(req: Request, res: Response){
-        try {
-            res.send("register successfully")
-        } catch (error) {
-            res.send(error)
-        }
+  async register(req: Request, res: Response) {
+    try {
+      res.send({ res, req })
+    } catch (error) {
+      res.send(error)
     }
+  }
 
-    async login(req: Request, res: Response){
-        try {
-            res.send("login successfully")
-        } catch (error) {
-            res.send(error)
-        }
+  async login(req: Request, res: Response) {
+    try {
+      res.send('login successfully')
+    } catch (error) {
+      res.send(error)
     }
+  }
 
-    async forgotPassword(req: Request, res: Response){
-        try {
-            res.send("forgotPassword successfully")
-        } catch (error) {
-            res.send(error)
-        }
+  async forgotPassword(req: Request, res: Response) {
+    try {
+      res.send('forgotPassword successfully')
+    } catch (error) {
+      res.send(error)
     }
+  }
 
-    async resetPassword(req: Request, res: Response){
-        try {
-            res.send("resetPassword successfully")
-        } catch (error) {
-            res.send(error)
-        }
+  async resetPassword(req: Request, res: Response) {
+    try {
+      res.send('resetPassword successfully')
+    } catch (error) {
+      res.send(error)
     }
+  }
 }
 
-export default UserController;
+export default UserController
