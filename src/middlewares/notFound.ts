a2a7bd8858bbc96b3f3b307c5c50f385e62bfdd5
kvargas-dev <kvargas@vumigroup.com>
2024-05-17 14:28:00 -0500

    delete individual middleware files

--- src/middlewares/notFound.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404).json({ error: error.message });
-};
