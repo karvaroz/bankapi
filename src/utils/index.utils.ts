a732a72a987311872668fddaccc21eff5b93a855
kvargas-dev <kvargas@vumigroup.com>
2024-05-24 08:49:38 -0500

    generateCode

--- src/utils/index.utils.ts
+++ src/utils/index.utils.ts
@@ -8,9 +8,18 @@ const handleSuccess = (res: Response, message: string, data = {}, statusCode: nu
   return res.status(statusCode).json({ status: true, message, data: { ...data } })
 }
 
+const generateCode = (num: number = 15) => {
+  const dateString = Date.now().toString(36)
+  const randomness = Math.random().toString(36).substr(2)
+  let result = randomness + dateString
+  result = result.length > num ? result.substring(0, num) : result
+  return result.toLocaleUpperCase()
+}
+
 const Utility = {
   handleError,
-  handleSuccess
+  handleSuccess,
+  generateCode
 }
 
 export default Utility
