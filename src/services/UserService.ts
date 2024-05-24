172b7305488c5e3e32cb8861d7ccbd488c3739b9
kvargas-dev <kvargas@vumigroup.com>
2024-05-24 15:24:47 -0500

    updateRecord

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -14,6 +14,11 @@ class UserService {
     const query = { where: { ...record }, raw: true } as IFindUserQuery
     return this.userDataSource.fetchOne(query)
   }
+
+  async updateRecord(searchBy: Partial<IUser>, record: Partial<IUser>): Promise<void> {
+    const query = { where: { ...searchBy } } as IFindUserQuery
+    await this.userDataSource.updateOne(query, record)
+  }
 }
 
 export default UserService
