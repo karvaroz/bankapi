29620fe3021973633f56933e0137d29a56d3f08e
kvargas-dev <kvargas@vumigroup.com>
2024-05-27 13:47:26 -0500

    AccountController

---
+++ src/controllers/AccountController.ts
@@ -0,0 +1,28 @@
+import { ResponseCode } from '../interfaces/enum/codeEnum'
+import AccountService from '../services/AccountService'
+import Utility from '../utils/index.utils'
+
+class AccountController {
+  private accountService: AccountService
+
+  constructor(_accountService: AccountService) {
+    this.accountService = _accountService
+  }
+
+  async createAccount(req: Request, res: Response) {
+    // try {
+    //   const params = { ...req.body }
+    //   const newAccount = {
+    //     userId: params.user.id,
+    //     type: params.type
+    //   }
+    //   let account = await this.accountService.createAccount(newAccount)
+    //   return Utility.handleSuccess(res, 'Account created successfully', { account }, ResponseCode.SUCCESS)
+    //   // return Utility.handleSuccess(res, 'Login successful', { user, token }, ResponseCode.SUCCESS)
+    // } catch (error) {
+    //   return Utility.handleError(res, (error as TypeError).message, ResponseCode.SERVER_ERROR)
+    // }
+  }
+}
+
+export default AccountController
