0c623714f902f3573ba0c7166f49ad4c87a68d9c
kvargas-dev <kvargas@vumigroup.com>
2024-05-27 15:37:47 -0500

    UserRole

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express'
 import { ResponseCode } from '../interfaces/enum/codeEnum'
 import AccountService from '../services/AccountService'
 import Utility from '../utils/index.utils'
