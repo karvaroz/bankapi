ca3018473aeb472b7680f80634d615fd1f3a9492
kvargas-dev <kvargas@vumigroup.com>
2024-05-27 16:26:52 -0500

    getAllUserAccounts

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -24,6 +24,29 @@ class AccountController {
       return Utility.handleError(res, (error as TypeError).message, ResponseCode.SERVER_ERROR)
     }
   }
+
+  async getAllUserAccounts(req: Request, res: Response) {
+    try {
+      const params = { ...req.body }
+      let accounts = await this.accountService.getAccountsByUserId(params.user.id)
+      return Utility.handleSuccess(res, 'Account fetched successfully', { accounts }, ResponseCode.SUCCESS)
+    } catch (error) {
+      return Utility.handleError(res, (error as TypeError).message, ResponseCode.SERVER_ERROR)
+    }
+  }
+
+  async getUserAccount(req: Request, res: Response) {
+    try {
+      const params = { ...req.params }
+      let account = await this.accountService.getAccountByField({ id: Utility.EscapeHtml(params.id) })
+      if (!account) {
+        return Utility.handleError(res, 'Account not found', ResponseCode.NOT_FOUND)
+      }
+      return Utility.handleSuccess(res, 'Account fetched successfully', { account }, ResponseCode.SUCCESS)
+    } catch (error) {
+      return Utility.handleError(res, (error as TypeError).message, ResponseCode.SERVER_ERROR)
+    }
+  }
 }
 
 export default AccountController
