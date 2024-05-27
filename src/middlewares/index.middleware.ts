6e2f3b55f62dc9cd528b01f9899aee85067c606f
kvargas-dev <kvargas@vumigroup.com>
2024-05-27 15:17:00 -0500

    Auth

--- src/middlewares/index.middleware.ts
+++ src/middlewares/index.middleware.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from 'express'
 import { Schema } from 'yup'
 import Utility from '../utils/index.utils'
 import { ResponseCode } from '../interfaces/enum/codeEnum'
+import { IUser } from '../interfaces/IUser'
+import jwt from 'jsonwebtoken'
+import { AccountStatus } from '../interfaces/enum/userEnum'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(`[${new Date().toISOString()}] ${req.method} ${req.url} - ${err.message}`)
@@ -23,6 +26,41 @@ export const validator = (schema: Schema<any>) => {
       next()
     } catch (error: any) {
       return Utility.handleError(res, error.errors[0], ResponseCode.BAD_REQUEST)
+    }
+  }
+}
+
+export const Auth = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let token: string = req.headers.authorization ?? ''
+      if (Utility.IsEmpty(token)) {
+        throw new TypeError('Authorization failed')
+      }
+      token = token.split(' ')[1]
+      const decode = jwt.verify(token, process.env.JWT_KEY as string) as IUser
+      if (decode && decode.id) {
+        const user = await userService.getUserByField({ id: decode.id })
+        if (!user) {
+          throw new TypeError('Authorization failed')
+        }
+
+        if (user.accountStatus == AccountStatus.DELETED) {
+          throw new TypeError('Account does not exist')
+        }
+
+        if (user.accountStatus == AccountStatus.SUSPENDED) {
+          throw new TypeError('Account suspended')
+        }
+
+        if (user.accountStatus == AccountStatus.FROZEN) {
+          throw new TypeError('Account frozen')
+        }
+      }
+      req.body.user = decode
+      next()
+    } catch (error) {
+      return Utility.handleError(res, (error as TypeError).message, ResponseCode.BAD_REQUEST)
     }
   }
 }
