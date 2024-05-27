52129afdacbeb41e537918f2cd3731b7d7bae93c
kvargas-dev <kvargas@vumigroup.com>
2024-05-27 13:47:19 -0500

    AccountDataSource

---
+++ src/database/datasources/accountDataSource.ts
@@ -0,0 +1,23 @@
+import { IFindAccountQuery, IAccount, IAccountCreationBody, IAccountDataSource } from '../../interfaces/IAccount'
+import AccountModel from '../../models/AccountModel'
+import { FindOptions } from 'sequelize'
+
+class AccountDataSource implements IAccountDataSource {
+  async create(record: IAccountCreationBody): Promise<IAccount> {
+    return await AccountModel.create(record)
+  }
+
+  async fetchOne(query: IFindAccountQuery): Promise<IAccount | null> {
+    return await AccountModel.findOne(query)
+  }
+
+  async updateOne(searchBy: IFindAccountQuery, data: Partial<IAccount>): Promise<void> {
+    await AccountModel.update(data, searchBy)
+  }
+
+  async fetchAll(query: FindOptions<IAccount>): Promise<IAccount[]> {
+    return await AccountModel.findAll(query)
+  }
+}
+
+export default AccountDataSource
