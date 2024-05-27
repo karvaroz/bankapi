673efe3f6eb9f2d6ec0471dd7ca22cdefe3e0dfb
kvargas-dev <kvargas@vumigroup.com>
2024-05-27 15:17:31 -0500

    AccountStatus

--- src/interfaces/enum/accountEnum.ts
+++ src/interfaces/enum/accountEnum.ts
@@ -1,3 +1,9 @@
+export const AccountType = {
+  SAVING_ACCOUNT: 'SAVING_ACCOUNT',
+  CURRENT_ACCOUNT: 'CURRENT_ACCOUNT',
+  FIXED_SAVING_ACCOUNT: 'FIXED_SAVING_ACCOUNT'
+}
+
 export const AccountStatus = {
   ACTIVE: 'ACTIVE',
   DORMANT: 'DORMANT',
